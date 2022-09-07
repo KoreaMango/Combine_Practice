117933f8929d5d54c360713e3ff0efcca0700dea
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-09-07 16:32:08 +0900

    proj init

---
+++ CombineAF/CombineAF/CombineAFApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineAFApp.swift
+//  CombineAF
+//
+//  Created by 강민규 on 2022/09/07.
+//
+
+import SwiftUI
+
+@main
+struct CombineAFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
