c09d37b51278bb1ff37ee9fccbac283f7f2f8660
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-09-09 22:14:34 +0900

    Constant Add

---
+++ CombineAPi/CombineAPi/Constant.swift
@@ -0,0 +1,17 @@
+//
+//  Constant.swift
+//  CombineAPi
+//
+//  Created by 강민규 on 2022/09/09.
+//
+
+import Foundation
+
+private let apiKey = "7d44cd3f14e73a7ec226c506bb668083"
+private let baseURL = "https://api.openweathermap.org"
+private let query = "/data/2.5/weather?q="
+private let apiQ = "&appid="
+private let imageQ = "/img/w/"
+
+let countries : [String] = [ "GongJu", "GwangJu", "Gumi", "Gunsan", "Daegu", "Daejeon", "Mokpo", "Busan", "Seosan", "Seoul", "Sokcho", "Suwon", "Suncheon", "Ulsan", "Iksan", "Jeonju", "Jeju", "Cheonan", "Cheongju", "Chuncheon" ]
+let countriesKR : [String] = [ "공주", "광주(전라남도)", "구미", "군산", "대구", "대전", "목포", "부산", "서산", "서울", "속초", "수원", "순천", "울산", "익산", "전주", "제주시", "천안", "청주", "춘천" ]
